810a365edf1ace54af90ed46345d27118c51ef46
conteluca <conteluca23@gmail.com>
2023-06-06 10:07:38 +0200

    Update Contact.js
    email update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -16,7 +16,7 @@ const Contact = ({lang}) => {
         console.log(data);
         e.preventDefault();
         setLoading(true);
-        emailjs.sendForm('service_uz55uzk',
+        emailjs.sendForm('service_5fq8kqy',
             'template_epmv7fi', form.current,
             'u6ft_vqteHGQUZKvs')
             .then((result) => {
